f76f34b91357a78c762b35150b0336a05610f133
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2023-10-10 23:49:26 +0530

    update

---
+++ bubble-game.js
@@ -0,0 +1,96 @@
+var timer = 10;
+var score = 0;
+var num = 0;
+
+function makebubble(){
+    var bubble = "";
+    for(var i = 1; i<=96;i++){
+    random = Math.floor(Math.random()*10)
+    bubble += `<div class="circle">${random}</div>`;
+}
+document.querySelector(".cbottom").innerHTML = bubble;
+
+}
+
+function runtimer(){
+    var a = setInterval(function(){
+        if (timer > 0){
+        timer--;
+        document.querySelector("#timerval").textContent = timer
+    }
+    if (timer == 0){
+        clearInterval(a);
+        
+        document.querySelector(".cbottom").innerHTML = `<h2>Game Over. Your Score is: ${score}</h2>`;
+    }  
+       
+    },1000);
+}
+
+function newhit(){
+    num = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = num;
+}
+
+function increasescore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+    
+};
+document.querySelector(".reset").onclick =() => {
+    timer = 9;
+    score = 0;
+    makebubble();
+    newhit();
+    eventbubbling();
+    runtimer();
+};
+
+            
+
+function eventbubbling(){
+    document.querySelector(".cbottom").addEventListener("click",function(details){
+        var targethit = (Number(details.target.textContent));
+        if (targethit === num){
+            increasescore();
+            makebubble();
+            newhit();
+        }
+    });
+}
+
+var c = 0;
+function darktheam(){
+    var dark = document.querySelector(".ctop")
+    var btn = document.querySelector("button")
+    var dar = document.querySelector(".cbottom")
+    var main = document.getElementById("main")
+
+    btn.addEventListener("click",function(){
+            if (c == 0){
+            dark.style.backgroundColor="rgb(187 111 142)";
+            dar.style.backgroundColor="#323237";
+            main.style.backgroundColor="#241f1f";
+            btn.innerHTML="Light Theam";
+            c = 1;
+        }
+        
+    else {
+        dark.style.backgroundColor="rgb(129 181 192)";
+                main.style.backgroundColor="white";
+                dar.style.backgroundColor="#292c46";
+                btn.innerHTML="Dark Theam";
+                c = 0;
+    }
+
+    
+    
+});
+}
+
+
+runtimer();
+makebubble();
+newhit();
+eventbubbling();
+darktheam();
